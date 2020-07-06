baa3e50c8873fe31206f82053e2ce5f538bc061a
JúDCraide <juliadcraide31@gmail.com>
2020-07-06 14:41:46 -0300

    JS Avançado II: aula 6 - ordenando a tabela pela coluna

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,7 +12,10 @@ class NegociacaoController {
       new ListaNegociacoes(),
       new NegociacoesView($('#negociacoes-view')),
       'adiciona',
-      'esvazia'
+      'esvazia',
+      //adicionamos o ordena eo inverte na lista de métodos q atualizam a view
+      'ordena',
+      'inverte'
     )
 
     this._mensagem = new Bind(
@@ -21,6 +24,8 @@ class NegociacaoController {
       'texto'
     )
 
+    //armazena como tabela está sendo ordenada
+    this._ordemAtual = '';
   }
 
   adiciona(event) {
@@ -29,7 +34,6 @@ class NegociacaoController {
     this._listaNegociacoes.adiciona(this._criaNegociacao());
     this._mensagem.texto = 'Negociação adicionada';
     this._limpaFormulario();
-
   }
 
   importaNegociacoes() {
@@ -41,7 +45,6 @@ class NegociacaoController {
         negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
         this._mensagem.texto = 'Negociações da semana retrasada importadas com sucesso.';
       }).catch(erro => this._mensagem.texto = erro);
-
   }
 
   apaga() {
@@ -65,4 +68,22 @@ class NegociacaoController {
     this._inputData.focus();
   }
 
+  //vamos criar um método para ordenar pela coluna
+  //ele vai ser chamado pelas colunas do negociacoesView
+  ordena(coluna){
+    //como apenas recebemos uma cópia da lista temos que criar um método ordena no model
+    //e passamos uma função que ordena crescentemente EX: a=5 e b=2, a-b=3 -> positivo -> [b,a]
+    //this._listaNegociacoes.ordena((a,b) => a[coluna]-b[coluna]);
+
+    //mas queremos que quando clicamos a segunda vez na coluna
+    //sua oredenação seja invertida(crescente ou decrescente)
+    if(coluna === this._ordemAtual){
+      //se a nova ordem for igual a ordemAtual invertemos a coluna
+      this._listaNegociacoes.inverte();
+    } else {
+      this._listaNegociacoes.ordena((a,b) => a[coluna]-b[coluna]);
+    }
+    //por fim atualizamos _ordemAtual
+    this._ordemAtual=coluna;
+  }
 }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,25 +1,35 @@
 class ListaNegociacoes {
 
-    constructor(armadilha){
+    constructor(armadilha) {
         this._negociacoes = [];
     }
 
-    adiciona(negociacao){
+    adiciona(negociacao) {
         this._negociacoes.push(negociacao);
     }
 
-    get negociacoes() {     
+    get negociacoes() {
         return [].concat(this._negociacoes);
     }
-  
+
     esvazia() {
         this._negociacoes = [];
     }
 
-    get volumeTotal(){
+    get volumeTotal() {
         return this._negociacoes.reduce(
-            (total, n) => total + n.volume, 0.0 
+            (total, n) => total + n.volume, 0.0
         )
     }
 
+    //criamos um método ordena que será chamado pelo controle
+    //ele recebe uma função que serve de critério para ordenação
+    ordena(critério) {
+        this._negociacoes.sort(critério);
+    }
+
+    //criamos uma função para inverter a lista
+    inverte() {
+        this._negociacoes.reverse();
+    }
 }
